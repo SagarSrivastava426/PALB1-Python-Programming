37c2088acdc91646bc88b467e1f2cb5fb1ec0849
SagarSrivastava426 <sagar.srivastava739@gmail.com>
2026-02-17 18:26:25 +0530

    Create subset.py

---
+++ subset.py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    def isSubset(self, a, b):
+      f={}
+      for i in a:
+        if if in f:
+          f[i]+=1
+        else:
+          f[i]=1
+      for i in b:
+        if i not in f or f[i]==0:
+          return False:
+      f[i]-=1
+
+      return False
