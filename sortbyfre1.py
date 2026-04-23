9310e9953a5071decfea1d1af12893d909e09406
SagarSrivastava426 <sagar.srivastava739@gmail.com>
2026-04-24 01:45:44 +0530

    Create sortbyfre1.py

---
+++ sortbyfre1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def frequencySort(self, s):
+        # code here
+        from collections import Counter
+        
+        freq = Counter(s)
+        
+        # sort by (frequency, character)
+        sorted_chars = sorted(freq.items(), key=lambda x: (x[1], x[0]))
+        
+        result = ""
+        
+        for ch, count in sorted_chars:
+            result += ch * count
+        
+        return result
+        
+        
